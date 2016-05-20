0edebbe618a9e9adcd7db60d14426d1359be45bb
Omry Ross <omriross@gmail.com>
2016-05-20 14:32:17 +0200

    Adding the bond payments and optionality into the double option bond

--- src/Web/Instrument/DoubleOptionBond.hs
+++ src/Web/Instrument/DoubleOptionBond.hs
@@ -21,7 +21,7 @@ import Fields
 {-@CODE@-}
 data DoubleOptionBond = DOB {
       price       :: Double
-    , frequency   :: Double -- How many times a year there is payment
+    , frequency   :: Int -- frquency of payments in working days
     , fvic        :: Double
     , fvbc        :: Double
     , margin      :: Double
@@ -35,73 +35,27 @@ doubleOptionBond =
             , url        = "doubleOptionBond" }
 
 
-iC = chosenBy ("IC", 0) -- The information about the Investor Decision is a global variablt
-bC = chosenBy ("BC", 0) -- same for Buyer Decision
+iC = chosenBy ("IC", 0) -- Buyer Decision to call
+bC = chosenBy ("BC", 0) --  Buyer Decision to call
 
 makeContract startDate optData =
     let fv = r 100
         ir = obs (rate optData, 0)
-        maturity = fromIntegral $ diffDays (endDate optData) startDate 
-        in doubleOptionOnBond fv (r (fvic optData)) (r (fvbc optData)) maturity
-
+        maturity = fromIntegral $ diffDays (endDate optData) startDate
+        bp = bondPayments ir (r (margin optData)) (frequency optData) maturity 
+        optionality = doubleOptionOnBond fv (r (fvic optData)) (r (fvbc optData)) maturity
+     in both  bp optionality
+-- some helper functions
 doubleOptionOnBond fv fvic fvbc maturity =
- --   let fv = 100 -- Face value usually 100
- --       fvic = 105 -- Face value if issuer calls - can be time dependedt
- --       fvbc = 95 -- Face value if buyer calls 
-   --     obsI = obs ("IC", 0)
-   --     obsB = obs ("BC", 0)
-   --     maturity = 10
     checkWithin (iC !|! bC)    maturity
-      -- if (obs ("IC", 0) !|! obs ("BC", 0) ) then    -- How do I do or operator ? What does $ means, how do I send a function as a parameter, when do I use (function p1 p2) or function p1 p2 
           ( iff iC
              (scale fvic (transfOne NOK "I" "B"))
              (scale fvbc (transfOne NOK "I" "B")) )
          (scale fv (transfOne NOK "I" "B"))
-         
--- some helper functions
-
 daysStep = daysStep' []
 daysStep' ds step c to = if (c  < to) then daysStep' (ds ++ [c]) step (c+step) to  else ds
-
 bondPayments ir margin frequency maturity  =  foreach ( daysStep frequency frequency maturity )
                                            (
                                            iff  (translExp (nott (iC !|! bC)) (-1) )
-                                           (scale ( margin + ir)   (transfOne NOK "I" "B")) -- we need to add here the margin
+                                           (scale ( margin + ir)   (transfOne NOK "I" "B")) 
                                            zero ) 
-
-
-{-
-
--- Stop / Step 2
-
---  This needs to be changed as the function is a function of the frequency (i.e. qarterly, semi annually, etc)
-
-onceAQuarter c = foreach [2,4,6,8,10] c
-
-
-bondPayments ir margin  = onceAQuarter (   iff  (translExp (nott (iC !|! bC)) (-1) )
-                                  (scale ( margin + ir)   (transfOne NOK "I" "B")) -- we need to add here the margin
-                                  zero )
-
-
-makeContract startDate optData =
-let ..
- in both (doubleOptionOnBond fv (fvic optData) (fvbc optData) maturity)  bondPayments (ir optData) (margin optData)
-
-doubleOptionBond = both doubleOptionOnBond  bondPayments
-
-
-
-
-makeContract startDate optData =
-    let
-        und1 = obs (underlying1 optData, 0)
-        und2 = obs (underlying2 optData, 0) 
-        maturity = fromIntegral $ diffDays (endDate optData) startDate 
-        strk = r $ strike optData
-        val = maxx 0 ((maxx und1 und2) - strk)
-    in
-      transl maturity $ scale val $ transfOne EUR "me" "you"
-
-
--}
